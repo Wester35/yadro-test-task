610ecc99e8b3a5edc9dec4f67fbc66936224d0b5
Wester35 <shum0302@gmail.com>
2025-05-20 11:40:45 +0300

    изменение модели и настроек
    теперь бд нормально может хранить пользователей, но id иногда пропускается т к используется автоинкремент

--- task/api/models.py
+++ task/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class Location(models.Model):
@@ -7,22 +8,29 @@ class Location(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
-    postcode = models.IntegerField()
+    postcode = models.CharField(max_length=20)
 
     def __str__(self):
         return f"{self.street_number} {self.street_name}, {self.city}"
 
 
 class User(models.Model):
-    external_id = models.IntegerField(unique=True)
     gender = models.CharField(max_length=6)
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=14)
+    phone_number = models.CharField(max_length=30)
     email = models.EmailField()
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     picture_thumbnail = models.URLField(blank=True)
-    slug = models.SlugField(unique=True, blank=True)
+    profile_url = models.URLField(unique=True, blank=True)
 
     def __str__(self):
-        return f"{self.name} {self.surname}"
+        return f"{self.name} {self.surname} ({self.id})"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if not self.profile_url:
+            base_url = getattr(settings, 'SITE_BASE_URL', 'http://127.0.0.1:8008')
+            self.profile_url = f"{base_url}/{self.id}"
+            super().save(update_fields=['profile_url'])
--- task/task/settings.py
+++ task/task/settings.py
@@ -117,7 +117,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+SITE_BASE_URL = "http://127.0.0.1:8000"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
