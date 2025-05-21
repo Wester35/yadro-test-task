8619f1097a103f2c338af8eee3d2fd7637142b21
Wester35 <shum0302@gmail.com>
2025-05-21 13:36:13 +0300

    изменение внешнего вида
    добавлена кнопка на профиль в таблице, также сделаны бутстрап стили для user_detail

--- task/frontend/tables.py
+++ task/frontend/tables.py
@@ -4,6 +4,12 @@ from api.models import User
 class UserTable(tables.Table):
     picture_thumbnail = tables.TemplateColumn('<img src="{{ record.picture_thumbnail }}" alt="thumbnail">')
 
+    link = tables.TemplateColumn(
+        '<a href="/{{ record.id }}/" class="btn btn-primary btn-sm">Перейти</a>',
+        orderable=False,
+        verbose_name='Link'
+    )
+
     class Meta:
         model = User
         template_name = "django_tables2/bootstrap4.html"
